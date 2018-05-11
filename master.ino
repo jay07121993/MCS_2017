cad16edc46498978e13a76abfc7980d4d4cd3297
Jaydeepkumar Gondaliya <gjg.gondaliya@gmail.com>
2018-05-11 13:42:01 +0200

    Master code for module distance and matrix

--- master.ino
+++ master.ino
@@ -1,183 +1,68 @@
-// --------------------Master Code for module status and Module distance-----------------------------------------//
+//----------------- Master code for Distance matrix------------------------// 
 
-#include <Wire.h>
+#include "RS485_protocol.h"
+#include <SoftwareSerial.h>
+const byte ENABLE_PIN = 2;
+SoftwareSerial rs485 (10, 11); // receive pin, transmit pin
 
-//byte i2cdata[3];
+char input[5];
+int charsRead = 0;
+boolean received = false;
+char res;
+boolean sendnow = false;
+void setup()
+{
 
-void setup() {
-  
-  Wire.begin();
-  Serial.begin(9600);
+Serial.begin(9600);
+rs485.begin (28800);
 
-   //Distance Matrix..............................
-   
-  //accessing data from slave 1
-   Wire.requestFrom(1, 3); //(slaves_address,no. of bytes requested)
-  
-  int a = Wire.available();
-  
-  Serial.print("Arduino - Modul 1 distance to other arduino: ");
-  Serial.println(a);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
+  pinMode (ENABLE_PIN, OUTPUT);
 
-  Serial.println();
- 
-  delay(500);
-  // accessing data from slave 2
-  
-  Wire.requestFrom(2, 1);
-  
-  int b = Wire.available();
-  
-  Serial.print("Arduino - Modul 2 distance to other arduino: ");
-  Serial.println(b);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
-
-  //memset(&i2cdata, 0, 3);
-
-  delay(500);
- 
-  //accessing data from slave 3
-  
-  Wire.requestFrom(3, 2);
-  
-  int c = Wire.available();
-  
-  Serial.print("Arduino Modul 3 distance to other arduino: ");
-  Serial.println(c);
-  while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
-
-  //memset(&i2cdata, 0, 3);
-
-  delay(500);
-  //accessing data from Slave 4
-  
-  Wire.requestFrom(4, 4);
-    
-  
-  int d = Wire.available();
-  
-  Serial.print("Arduino - Modul 4 distance to other arduino: ");
-  Serial.println(d);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
-  delay(500);
- 
- //accessing data from Slave 5
-  Wire.requestFrom(5, 3);
-    
-  
-  int e = Wire.available();
-  
-  Serial.print("Arduino - Modul 5 distance to other arduino: ");
-  Serial.println(e);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
-delay(500);
-
-
-//---------------------------Master code for module-status matrix-----------------------------------//
-   //slave 1
-   Wire.requestFrom(1, 6);
-    
-  
-  int a = Wire.available();
-  
-  Serial.print("\nArduino 1 in line with Arduino(M) and Arduino 3: ");
-  Serial.println(a);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-   //slave 2
-   Wire.requestFrom(2, 6);
-    
-  
-  int b = Wire.available();
-  
-  Serial.print("\nArduino 2 in line with Arduino(M) and Arduino 1 : ");
-  Serial.println(b);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
- 
-  delay(500);
-
-   //slave 3
-   Wire.requestFrom(3, 6);
-    
-  
-  int c = Wire.available();
-  
-  Serial.print("\nArduino 3 in line with Arduino(M) and Arduino 1: ");
-  Serial.println(c);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
- 
-  delay(500);
-
-   //slave 4
-   Wire.requestFrom(4, 6);
-    
-  
-  int d = Wire.available();
-  
-  Serial.print("\nArduino 4 in line with Arduino(M) and Arduino 2: ");
-  Serial.println(d);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-  Serial.println();
- 
-  delay(500);
-
-   //slave 5
-   Wire.requestFrom(5, 6);
-    
-  
-  int e = Wire.available();
-  
-  Serial.print("\nArduino 5 in line with and Arduino 1: ");
-  Serial.println(e);
-    while (Wire.available()) {
-    Serial.print(Wire.read());
-    Serial.print(" ");
-  } 
-
-  Serial.println();
- 
-  delay(500);
- 
 }
 
-void loop() {}
+void loop()
+{
+
+Serial.flush();
+if (Serial.available() == 0)
+{
+
+}
+delay(200);
+
+while (Serial.available() > 0)
+{
+
+charsRead = Serial.readBytesUntil('\n', input, sizeof(input) - 1); 
+sendnow = true;
+
+}
+if (sendnow)
+{
+digitalWrite (ENABLE_PIN, HIGH); // enable sending
+rs485.write(input);
+Serial.println(input);
+//delayMicroseconds (660);
+digitalWrite (ENABLE_PIN, LOW);
+sendnow = false;
+}
+
+delay(1000);
+digitalWrite (ENABLE_PIN, LOW);
+
+while (rs485.available() && !sendnow)
+{
+Serial.println("Receiving Distance Matrix");
+int dist;
+
+for (int i = 0; i < 5; i++)
+{
+dist = rs485.read();
+digitalWrite (ENABLE_PIN, HIGH);
+
+Serial.println(dist);
+  }
+ }
+}
+
+//---------------------------- Master code for Module status Matrix-------------------------------------//
